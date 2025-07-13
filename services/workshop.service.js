41e3aad6368052c3cab30e62ec98b36646c9f465
Ali-Tharwat-181 <alitharwat358@gmail.com>
2025-07-13 09:20:50 +0300

    deleted unwanted file, fixed a simple importing error

--- services/workshop.service.js
+++ services/workshop.service.js
@@ -1,43 +1,40 @@
 // workshop.service.js
-import Workshop from '../models/Workshop.js';
+import Workshop from "../models/Workshop.js";
 
 export const getAllWorkshops = async () => {
-    return await Workshop.find().populate('mentor registeredStudents');
+  return await Workshop.find().populate("mentor registeredStudents");
 };
 
 export const createWorkshop = async (data) => {
-    const newWorkshop = new Workshop(data);
-    return await newWorkshop.save();
+  const newWorkshop = new Workshop(data);
+  return await newWorkshop.save();
 };
 
 export const getWorkshopById = async (id) => {
-    return await Workshop.findById(id).populate('mentor registeredStudents');
+  return await Workshop.findById(id).populate("mentor registeredStudents");
 };
 
 export const updateWorkshop = async (id, updates) => {
-    return await Workshop.findByIdAndUpdate(id, updates, { new: true });
+  return await Workshop.findByIdAndUpdate(id, updates, { new: true });
 };
 
 export const deleteWorkshop = async (id) => {
-    return await Workshop.findByIdAndDelete(id);
+  return await Workshop.findByIdAndDelete(id);
 };
 
-import Workshop from '../models/Workshop.js';
-
 export const registerStudentToWorkshop = async (workshopId, studentId) => {
-    const workshop = await Workshop.findById(workshopId);
+  const workshop = await Workshop.findById(workshopId);
 
-    if (!workshop) throw new Error('Workshop not found');
+  if (!workshop) throw new Error("Workshop not found");
 
+  if (workshop.registeredStudents.includes(studentId)) {
+    throw new Error("Student already registered in this workshop");
+  }
 
-    if (workshop.registeredStudents.includes(studentId)) {
-        throw new Error('Student already registered in this workshop');
-    }
+  if (workshop.registeredStudents.length >= workshop.capacity) {
+    throw new Error("Workshop is full");
+  }
 
-    if (workshop.registeredStudents.length >= workshop.capacity) {
-        throw new Error('Workshop is full');
-    }
-
-    workshop.registeredStudents.push(studentId);
-    return await workshop.save();
+  workshop.registeredStudents.push(studentId);
+  return await workshop.save();
 };
