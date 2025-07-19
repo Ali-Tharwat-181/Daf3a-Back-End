a531a9febf4cc5763695a0bdd9e08c6c8eb5fde4
Ali-Tharwat-181 <alitharwat358@gmail.com>
2025-07-19 22:21:24 +0300

    populate fileds in ai service

--- services/ai.service.js
+++ services/ai.service.js
@@ -4,7 +4,11 @@ import Workshop from "../models/Workshop.js";
 import { getRecommendationsFromGemini } from "../utils/recommendation.service.js";
 
 export const fetchRecommendationsForStudent = async (student) => {
-  const mentors = await User.find({ role: "mentor" });
-  const workshops = await Workshop.find();
+  const mentors = await User.find({ role: "mentor" }).populate(
+    "expertise links experience languages availability rating price verified"
+  );
+  const workshops = await Workshop.find().populate(
+    "registeredStudents title description date time location type price language image rating topic capacity mentor"
+  );
   return await getRecommendationsFromGemini(student, mentors, workshops);
 };
