376a85a60f7d38deac9bf917130d8c4e526c054f
Ali-Tharwat-181 <alitharwat358@gmail.com>
2025-07-23 20:42:18 +0300

    handle reporting system endpoints

---
+++ services/report.service.js
@@ -0,0 +1,46 @@
+// services/report.service.js
+import Report from "../models/report.model.js";
+
+export const createReportService = async ({
+  reporter,
+  reportedUser,
+  booking,
+  reason,
+  message,
+}) => {
+  const newReport = new Report({
+    reporter,
+    reportedUser,
+    booking,
+    reason,
+    message,
+  });
+
+  await newReport.save();
+  return { message: "Report submitted." };
+};
+
+export const getAllReportsService = async () => {
+  const reports = await Report.find()
+    .populate("reporter", "name role")
+    .populate("reportedUser", "name role")
+    .populate("booking");
+
+  return reports;
+};
+
+export const markReportResolvedService = async (reportId) => {
+  const report = await Report.findById(reportId);
+  if (!report) {
+    throw new Error("Report not found.");
+  }
+
+  if (report.status === "resolved") {
+    throw new Error("Report is already resolved.");
+  }
+
+  report.status = "resolved";
+  await report.save();
+
+  return { message: "Report marked as resolved." };
+};
